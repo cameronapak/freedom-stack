b89f4c547175841c97093109b57853a420b8c358
Cameron Pak <cameronandrewpak@gmail.com>
2024-02-17 09:35:39 -0600

    added pocketbase

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,14 @@
 interface Window {
   Alpine: import('alpinejs').Alpine;
 }
+
+// https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
+interface ImportMetaEnv {
+  readonly PB_USERNAME: string;
+  readonly PB_PASSWORD: string;
+  readonly PB_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
