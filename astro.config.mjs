6a0004adf9135ee02da7576b500ed37ba931ddb9
Cameron Pak <cameronandrewpak@gmail.com>
2024-02-16 23:40:46 -0600

    added htmx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+import htmx from "astro-htmx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), htmx()]
 });
