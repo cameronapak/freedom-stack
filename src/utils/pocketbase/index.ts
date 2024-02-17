cf386d2f11870e0955b1df87cb43f9169fb41726
Cameron Pak <cameronandrewpak@gmail.com>
2024-02-17 09:47:35 -0600

    added perios

--- src/utils/pocketbase/index.ts
+++ src/utils/pocketbase/index.ts
@@ -2,11 +2,11 @@ import PocketBase from "pocketbase";
 
 const pb = new PocketBase(import.meta.env.PB_URL);
 
-// This optimization ensures that you only re-authenticate when necessary, reducing the load on your PocketBase server
+// This optimization ensures that you only re-authenticate when necessary, reducing the load on your PocketBase server.
 export const authorizePB = async () => {
   if (!pb.authStore.isValid) {
     await pb.admins.authWithPassword(import.meta.env.PB_USERNAME, import.meta.env.PB_PASSWORD);
   }
-  
+
   return pb;
 };
