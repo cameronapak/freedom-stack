fbea744b8f28ac97b386e224fc8d15b1145ae466
Cameron Pak <cameronandrewpak@gmail.com>
2024-02-18 09:36:48 -0600

    added Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import htmx from "astro-htmx";
 import netlify from "@astrojs/netlify";
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), htmx()],
+  integrations: [tailwind(), alpinejs(), htmx(), sentry()],
   output: "server",
   adapter: netlify()
 });
