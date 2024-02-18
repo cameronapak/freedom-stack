d33983bd32b9c1f15bd14720e92ab862c3dfffa0
Cameron Pak <cameronandrewpak@gmail.com>
2024-02-18 09:47:10 -0600

    added Sentry integration easily

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,29 @@ import htmx from "astro-htmx";
 import netlify from "@astrojs/netlify";
 import sentry from "@sentry/astro";
 
+const SENTRY_DSN = import.meta.env.SENTRY_DSN;
+const SENTRY_AUTH_TOKEN = import.meta.env.SENTRY_AUTH_TOKEN;
+const SENTRY_PROJECT = import.meta.env.SENTRY_PROJECT;
+
+const integrations = [
+  tailwind(),
+  alpinejs(),
+  htmx(),
+]
+
+// To enable Sentry monitoring, add the following environment variables.
+// Learn more at https://docs.sentry.io/platforms/javascript/guides/astro/#prerequisites.
+if (SENTRY_DSN && SENTRY_AUTH_TOKEN && SENTRY_PROJECT) {
+  integrations.push(sentry({
+    dsn: SENTRY_DSN,
+    auth: SENTRY_AUTH_TOKEN,
+    project: SENTRY_PROJECT
+  }));
+} 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), htmx(), sentry()],
+  integrations,
   output: "server",
   adapter: netlify()
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -6,9 +6,18 @@ interface Window {
 
 // https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
 interface ImportMetaEnv {
+  /** Pocketbase API username */
   readonly PB_USERNAME: string;
+  /** Pocketbase API password */
   readonly PB_PASSWORD: string;
+  /** Pocketbase API URL */
   readonly PB_URL: string;
+  /** https://docs.sentry.io/platforms/javascript/guides/astro/#configure */
+  readonly SENTRY_DSN: string;
+  /** https://docs.sentry.io/platforms/javascript/guides/astro/#configure */
+  readonly SENTRY_AUTH_TOKEN: string;
+  /** https://docs.sentry.io/platforms/javascript/guides/astro/#configure */
+  readonly SENTRY_PROJECT: string;
 }
 
 interface ImportMeta {
