04d112c97c35245a12f0ce3567f27c5bf7bab909
Cameron Pak <cameronandrewpak@gmail.com>
2024-02-18 09:34:54 -0600

    added Netlify support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import htmx from "astro-htmx";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), htmx()]
+  integrations: [tailwind(), alpinejs(), htmx()],
+  output: "server",
+  adapter: netlify()
 });
