266e0b071a4fa2480ab746c35123fd116ca23b47
Cameron Pak <cameronandrewpak@gmail.com>
2024-06-22 19:59:32 -0500

    updated pocketbase information

--- src/utils/pocketbase/index.ts
+++ src/utils/pocketbase/index.ts
@@ -1,12 +1,20 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase(import.meta.env.PB_URL);
-
-// This optimization ensures that you only re-authenticate when necessary, reducing the load on your PocketBase server.
-export const authorizePB = async () => {
-  if (!pb.authStore.isValid) {
-    await pb.admins.authWithPassword(import.meta.env.PB_USERNAME, import.meta.env.PB_PASSWORD);
-  }
-
-  return pb;
+type PBLocals = {
+  pb: PocketBase;
 };
+
+export async function signIn(locals: PBLocals, email: string, password: string) {
+  const { token, record } = await locals.pb.collection('users').authWithPassword(email, password);
+  return { token, record };
+};
+
+export async function signOut(locals: PBLocals) {
+  locals.pb.authStore.clear();
+}
+
+export async function getUser(locals: PBLocals, email: string) {
+  return locals.pb.collection('users').getFullList(1, {
+    filter: `email = "${email}"`
+  });
+}
