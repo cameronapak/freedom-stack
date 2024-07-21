16cafad114812a8d6bf69aa5e29eda5b7ebafeb4
Cameron Pak <cameronandrewpak@gmail.com>
2024-07-21 14:22:27 -0500

    got TinyMCE added and also packages fixed

--- src/integrations.ts
+++ src/integrations.ts
@@ -1,29 +1,35 @@
 import type { AstroIntegration } from "astro";
 
+async function cpPkg(sourceDir: string, destDir: string) {
+  const fs = await import('fs');
+  const path = await import('path');
+
+  const sourcePath = path.resolve(sourceDir);
+  const destinationPath = path.resolve(destDir);
+
+  // Ensure the public directory exists
+  if (!fs.existsSync(path.dirname(destinationPath))) {
+    fs.mkdirSync(path.dirname(destinationPath), { recursive: true });
+  }
+
+  // Copy entire directory from node_modules to public directory
+  fs.cpSync(sourcePath, destinationPath, { recursive: true });
+
+  // Log the successful copy
+  console.log(`${sourcePath} has been copied to the public folder.`);
+}
+
 // Self-host TinyMCE so that it's allowed to be on the free-tier.
 export function copyTinymceToPublic(): AstroIntegration {
-    return {
-        name: "copy-tinymce-to-public",
-        hooks: {
-            "astro:build:setup": async ({ updateConfig }) => {
-                const fs = await import('fs');
-                const path = await import('path');
-
-                // Define the source and destination paths
-                const sourcePath = path.resolve('./node_modules/tinymce/');
-                const destinationPath = path.resolve('./public/tinymce/');
-
-                // Ensure the public directory exists
-                if (!fs.existsSync(path.dirname(destinationPath))) {
-                    fs.mkdirSync(path.dirname(destinationPath), { recursive: true });
-                }
-
-                // Copy entire directory from node_modules to public directory
-                fs.cpSync(sourcePath, destinationPath, { recursive: true });
-
-                // Log the successful copy
-                console.log('tinymce has been copied to the public folder.');
-            }
-        }
+  return {
+    name: "copy-tinymce-to-public",
+    hooks: {
+      "astro:config:setup": async () => {
+        await cpPkg('./node_modules/tinymce/', './public/tinymce/');
+      },
+      "astro:build:setup": async () => {
+        await cpPkg('./node_modules/tinymce/', './public/tinymce/');
+      }
     }
+  }
 }
