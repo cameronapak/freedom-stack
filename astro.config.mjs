c6604f58ef2c6662c6be7cfe0caeaa621dc43f70
Cameron Pak <cameronandrewpak@gmail.com>
2024-11-17 08:35:27 -0600

    rm sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,31 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import netlify from "@astrojs/netlify";
-import sentry from "@sentry/astro";
 import db from "@astrojs/db";
 import clerk from "@clerk/astro";
 import { copyTinymceToPublic } from "./src/integrations.ts";
-
-const SENTRY_DSN = import.meta.env.SENTRY_DSN;
-const SENTRY_AUTH_TOKEN = import.meta.env.SENTRY_AUTH_TOKEN;
-const SENTRY_PROJECT = import.meta.env.SENTRY_PROJECT;
-const isSentryEnabled = SENTRY_DSN && SENTRY_AUTH_TOKEN && SENTRY_PROJECT;
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     db(),
-    tailwind(), // This entrypoint file is where Alpine plugins are registered.
+    tailwind(),
     alpinejs({
       entrypoint: "/src/entrypoint"
-    }), // To enable Sentry monitoring, add the following environment variables.
-    // Learn more at https://docs.sentry.io/platforms/javascript/guides/astro/#prerequisites.
-    isSentryEnabled &&
-      sentry({
-        dsn: SENTRY_DSN,
-        auth: SENTRY_AUTH_TOKEN,
-        project: SENTRY_PROJECT
-      }),
+    }),
     copyTinymceToPublic(),
     clerk()
   ],
--- src/env.d.ts
+++ src/env.d.ts
@@ -22,20 +22,14 @@ declare global {
 
 // https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
 interface ImportMetaEnv {
-  /** https://docs.sentry.io/platforms/javascript/guides/astro/#configure */
-  readonly SENTRY_DSN: string;
-  /** https://docs.sentry.io/platforms/javascript/guides/astro/#configure */
-  readonly SENTRY_AUTH_TOKEN: string;
-  /** https://docs.sentry.io/platforms/javascript/guides/astro/#configure */
-  readonly SENTRY_PROJECT: string;
   /** https://docs.astro.build/en/guides/astro-db/#libsql */
-  readonly ASTRO_DB_REMOTE_URL: string;
+  readonly ASTRO_DB_REMOTE_URL?: string;
   /** https://docs.astro.build/en/guides/astro-db/#libsql */
-  readonly ASTRO_DB_APP_TOKEN: string;
+  readonly ASTRO_DB_APP_TOKEN?: string;
   /** https://clerk.com/docs/deployments/clerk-environment-variables#clerk-environment-variables */
-  readonly PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+  readonly PUBLIC_CLERK_PUBLISHABLE_KEY?: string;
   /** https://clerk.com/docs/deployments/clerk-environment-variables#clerk-environment-variables */
-  readonly CLERK_SECRET_KEY: string;
+  readonly CLERK_SECRET_KEY?: string;
 }
 
 interface ImportMeta {
