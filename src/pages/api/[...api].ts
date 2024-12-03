4b857a1929228a6a987037b9a462351e6ecddb89
Cameron Pak <cameronandrewpak@gmail.com>
2024-12-03 07:16:37 -0600

    Refactor API configuration in [...api].ts
    - Introduced a new configuration object for the application, enhancing the setup process with a structured initial configuration that includes versioning and authentication settings.
    - Updated the serve function to utilize the new configuration, improving maintainability and clarity in the API setup.
    - Added logging for the created application configuration to facilitate debugging and verification of settings.
    
    These changes streamline the API configuration and improve the overall structure of the code.

--- src/pages/api/[...api].ts
+++ src/pages/api/[...api].ts
@@ -1,13 +1,31 @@
 import { serve } from "bknd/adapter/astro";
+import { type ModuleConfigs, type CreateAppConfig, getDefaultConfig } from "bknd";
 
-export const prerender = false;
+const config = getDefaultConfig();
+const initialConfig: { version: number } & ModuleConfigs = {
+  version: 7, // just the latest version, configs get auto migrated
+  // config goes here. You can either use the default config and extend it,
+  // or just inspect the "__bknd" table (check for entries of "type" = "config")
+  ...config,
+  auth: {
+    ...config.auth,
+    enabled: true
+  }
+};
 
-export const ALL = serve({
+const createAppConfig: CreateAppConfig = {
   connection: {
     type: "libsql",
     config: {
       url: import.meta.env.ASTRO_DB_REMOTE_URL,
       authToken: import.meta.env.ASTRO_DB_APP_TOKEN
     }
-  }
-});
+  },
+  initialConfig
+};
+
+console.log(JSON.stringify(createAppConfig, null, 2));
+
+export const prerender = false;
+
+export const ALL = serve(createAppConfig);
