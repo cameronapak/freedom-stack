1ef4ad880e742ea4add34ebdf69c0540aea2f27b
Cameron Pak <cameronandrewpak@gmail.com>
2025-01-05 15:41:34 -0600

    rm better auth and astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import netlify from "@astrojs/netlify";
-import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    db(),
     tailwind(),
     alpinejs({
       entrypoint: "/src/entrypoint"
--- src/lib/auth-client.ts
+++
@@ -1,5 +0,0 @@
-import { createAuthClient } from "better-auth/client";
-
-export const client = createAuthClient({
-  baseURL: import.meta.env.BETTER_AUTH_URL
-});
