72fc99e8d7faa8388d3e1329efe7f9dd09dd8833
Cameron Pak <cameronandrewpak@gmail.com>
2025-01-05 16:50:16 -0600

    Added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { defineMiddleware } from "astro:middleware";
+import { getApi } from "bknd/adapter/astro";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const api = getApi(context, { mode: "dynamic" });
+  const user = await api.getUser();
+
+  console.log(user);
+
+  if (user) {
+    context.locals.user = user;
+  } else {
+    context.locals.user = null;
+  }
+
+  return next();
+});
