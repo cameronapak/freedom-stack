74902d5cae99ddf7d18a16439e5c28130a0c0445
Cameron Pak <cameronandrewpak@gmail.com>
2025-01-05 20:46:20 -0600

    Enhance API schema and authentication configuration
    - Introduced a new schema for posts and comments using the bknd data model, defining required fields and relationships.
    - Updated the API server configuration to enable authentication with JWT, including a random secret for security.
    - Added initial data seeding for posts to improve the setup process.

--- src/pages/api/[...api].ts
+++ src/pages/api/[...api].ts
@@ -1,6 +1,30 @@
+import type { ModuleBuildContext } from "bknd";
 import { serve } from "bknd/adapter/astro";
+import { em, entity, text, number } from "bknd/data";
+import { randomString } from "bknd/utils";
 
-export const prerender = false;
+const schema = em(
+  {
+    posts: entity("posts", {
+      // "id" is automatically added
+      title: text().required(),
+      slug: text().required(),
+      content: text(),
+      views: number()
+    }),
+    comments: entity("comments", {
+      content: text()
+    })
+
+    // relations and indices are defined separately.
+    // the first argument are the helper functions, the second the entities.
+  },
+  ({ relation, index }, { posts, comments }) => {
+    relation(comments).manyToOne(posts);
+    // relation as well as index can be chained!
+    index(posts).on(["title"]).on(["slug"], true);
+  }
+);
 
 export const ALL = serve({
   connection: {
@@ -9,6 +33,23 @@ export const ALL = serve({
       // location of your local Astro DB
       // make sure to use a remote URL in production
       url: "file:.astro/content.db"
+    }
+  },
+  initialConfig: {
+    auth: {
+      enabled: true,
+      jwt: {
+        secret: randomString(64)
+      }
+    },
+    data: schema.toJSON()
+  },
+  options: {
+    seed: async (ctx: ModuleBuildContext) => {
+      await ctx.em.mutator("posts").insertMany([
+        { title: "First post", slug: "first-post", content: "..." },
+        { title: "Second post", slug: "second-post", content: "..." }
+      ]);
     }
   }
 });
